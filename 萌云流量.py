68ee46c2d3a864e4a9b276f08f494f0019064f93
凉风 <87523187+LiangFeng111@users.noreply.github.com>
2023-03-31 17:37:18 +0800

    Add files via upload

---
+++ 萌云流量.py
@@ -0,0 +1,33 @@
+import requests
+
+headers1 = {
+    'User-Agent': 'Apipost client Runtime/+https://www.apipost.cn/',
+}
+
+data1 = {
+  'email': '1637465932@qq.com',
+  'passwd': 'psyah453'
+}
+
+response1 = requests.post('https://www.cutecloud.net/auth/login', headers=headers1, data=data1)
+cookies = response1.cookies.items()
+cookie = ''
+for name, value in cookies:
+    cookie += '{0}={1};'.format(name, value)
+    
+headers2 = {
+    'User-Agent': 'Apipost client Runtime/+https://www.apipost.cn/',
+    'cookie': cookie,    
+}
+
+data2 = {
+  'coupon': '',
+  'shop': '22',
+  'autorenew': '0',
+  'disableothers': '1'
+}
+
+response2 = requests.post('https://www.cutecloud.net/user/buy', headers=headers2, data=data2)
+json =  response2.json()
+print(json)
+input()
